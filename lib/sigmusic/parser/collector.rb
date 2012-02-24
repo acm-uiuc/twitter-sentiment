e58a261cc6c01621c002b1c4491743f63bc96efc
scowalt <xscowalt45x@gmail.com>
2012-02-24 12:37:51 -0600

    Removed redundant TextMood methods from collector.rb

--- lib/sigmusic/parser/collector.rb
+++ lib/sigmusic/parser/collector.rb
@@ -1,7 +1,8 @@
 print "Loading Twitter\n"
 require 'twitter'
-
 print "Done!\n"
+require "./textmood.rb"
+require "./facerecon.rb"
 
 #GOLBAL VARIABLES
 phrase = "#WhyDoPeopleThink"
@@ -12,7 +13,8 @@ def sendt(user = nil, image = nil, tweet = nil)
   #print "Found tweet!\nGathering info\n"
   #uI = userInfo(user)
   #iA = imageAnalysis(image)
-  tM = tweetMood(tweet)
+  tM = TextMood.score(tweet)
+  print "Score: ", score, "\n"
   #cS = calmScore(tweet)
   #arr = userInfo(user) + imageAnalysis(image) + [tweetMood(tweet)]
   #sendData(arr)
@@ -45,75 +47,6 @@ end
 #Low score is calm, high score is excited
 def calmScore(tweet = nil)
   #TODO
-end
-
-#Returns an integer representation of the mood of a string of text
-def tweetMood(tweet = nil)
-  #TODO Account for punctuation after words
-  print "Original tweet: " + tweet + "\n"
-
-  file = File.new("AFINN-111.txt", "r")
-  afinn = file.readlines() #string array
-  score = 0
-
-  tweetArr = textToArray(tweet)
-
-  #Compares each word to the AFINN, tallies the  score
-  length = tweetArr.length
-  n = 0
-  while (n<length)
-    word = tweetArr[n]
-    wordScore = Integer(searchScoreW(afinn,word))
-    #print "Word: \"",word,"\", Score: ",wordScore,"\n"
-    score += wordScore
-    n += 1
-  end
-  print "Score = ", score, "\n"
-  return score
-end
-
-#Takes a string and turns it into a string array
-def textToArray (tweet)
-  length = tweet.length()
-    n = 0
-    wordStart = 0
-    tweetArr = []
-    while (n < length)
-      if(tweet[n] == ' ' || n == length - 1)
-        if (n == length - 1) #fixes off by one at end
-          n+=1
-        end
-        tweetArr += [tweet[wordStart..n-1]]
-        wordStart = n+1
-      end
-      n+=1
-    end
-    tweetArr
-end
-
-#Returns the mood score of a given word (wrapper)
-def searchScoreW(arr = nil, word = nil)
-  if(word != nil && arr != nil)
-    word.downcase!
-    searchScore(arr,word,0,arr.length-1)
-  end #word not nil
-end #searchScore
-
-#Returns the modd score of a given word (recursive binary search)
-def searchScore(arr, word, lo, hi)
-  return 0 if (hi<lo)
-  mid = ((lo+hi)/2)
-  #print "mid = ",mid,"\n"
-  arrLine = arr[mid]
-  stop = arrLine.index("\t")
-  arrWord = arrLine[0..(stop-1)]
-  if(arrWord.==(word))
-    arrLine[stop+1..arrLine.length()-1]
-  elsif(arrWord.<(word))
-    searchScore(arr, word, mid+1, hi)
-  elsif(arrWord.>(word))
-    searchScore(arr, word, lo, mid-1)
-  end
 end
 
 print "Beginning search loop\n"
