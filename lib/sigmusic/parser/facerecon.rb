99613683a7ce7611e2c94e5889fc6a98eb203af0
Scott Walters <srwalte2@illinois.edu>
2012-02-23 23:05:10 -0600

    Turned facerecon.rb into a usable module

--- lib/sigmusic/parser/facerecon.rb
+++ lib/sigmusic/parser/facerecon.rb
@@ -1,30 +1,26 @@
-print "Starting program\n"
 require 'face'
-
-#GLOBAL VARIABLES
-smileface = 'http://www.esquire.com/cm/esquire/images/FK/george-clooney-smile-WI-1009-lg.jpg'
-frownface = 'http://1.bp.blogspot.com/_gjr6QvNeMBA/R1BPauUkFaI/AAAAAAAAAx8/MfNs5PV768Q/s1600-R/O%27Reilly+frown.bmp'
-#END GLOBAL VARIABLES
-
-#Gets API key and secret from file (lines 0 and 1)
-secretsfile = File.new("scowalt-secrets.txt")
-secrets = secretsfile.readlines
-apikey = secrets[0][12..secrets[0].length-2]
-apisecret = secrets[1][12..secrets[1].length-1]
-
-#Gets sentiment information on the smileface and frownface
-client = Face.get_client(:api_key => apikey, :api_secret => apisecret)
-results = client.faces_detect(:urls => [smileface,frownface])
+module FaceRecon
+#JSON PARSING:
 #results["photos"][photo number]["tags"][face number]["attributes"]["smiling"]["value" or "confidence"]
 
-smileresults = results["photos"][0]
-frownresults = results["photos"][1]
+#Returns the client needed to make searchs
+#Intended to be a private method
+def self.makeClient()
+  #Gets API key and secret from file (lines 0 and 1)
+  secretsfile = File.new("./scowalt-secrets.txt")
+  secrets = secretsfile.readlines
+  apikey = secrets[0][12..secrets[0].length-2]
+  apisecret = secrets[1][12..secrets[1].length-1]
+  Face.get_client(:api_key => apikey, :api_secret => apisecret)
+end
 
-print "Smile face smiling? ", results["photos"][0]["tags"][0]["attributes"]["smiling"]["value"], "\n"
-print "Smile face confidence: ", results["photos"][0]["tags"][0]["attributes"]["smiling"]["confidence"], "\n"
-print "Frown face smiling? ", frownresults["tags"][0]["attributes"]["smiling"]["value"], "\n"
-print "Frown face confience: ", frownresults["tags"][0]["attributes"]["smiling"]["confidence"], "\n"
-
-#print results
-print "\nDONE"
-gets
+#Returns an array (info about the first detected face): [boolean, integer]
+#Boolean: Smiling?
+#Integer: Confidence
+def self.smileInfo (imgurl = "")
+  client = FaceRecon.makeClient()
+  results = client.faces_detect(:urls => [imgurl])
+  results = results["photos"][0]["tags"][0]["attributes"]["smiling"]
+  [results["value"], results["confidence"]]
+end
+end #end of module
