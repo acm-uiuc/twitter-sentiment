64082d527d785b7f7f4f7d94f93cff10e3f8fcc3
Scott Walters <srwalte2@illinois.edu>
2012-02-25 19:04:34 -0600

    Minor changes to face_recon.rb

--- lib/twitter-sentiment/parser/face_recon.rb
+++ lib/twitter-sentiment/parser/face_recon.rb
@@ -1,13 +1,14 @@
 require 'face'
 module TwitterSentiment
   module Parser
-    module FaceRecon
+    class FaceRecon
       #JSON PARSING:
       #results["photos"][photo number]["tags"][face number]["attributes"]["smiling"]["value" or "confidence"]
       
       #Returns the client needed to make searchs
-      #Intended to be a private method
-      def self.makeClient()
+      #
+      #@private
+      def makeClient
         #Gets API key and secret from file (lines 0 and 1)
         secretsfile = File.new("./scowalt-secrets.txt")
         secrets = secretsfile.readlines
@@ -15,12 +16,14 @@ module TwitterSentiment
         apisecret = secrets[1][12..secrets[1].length-1]
         Face.get_client(:api_key => apikey, :api_secret => apisecret)
       end
+      private :makeClient
       
-      #Returns an array (info about the first detected face): [boolean, integer]
-      #Boolean: Smiling?
-      #Integer: Confidence
-      def self.smileInfo (imgurl = "")
-        client = FaceRecon.makeClient()
+      #Returns an array (info about the first detected face)
+      #
+      #@param [String] Image url
+      #@return [[boolean,int]] Smiling?, confidence
+      def smileInfo imgurl = ""
+        client = makeClient
         results = client.faces_detect(:urls => [imgurl])
         results = results["photos"][0]["tags"][0]["attributes"]["smiling"]
         [results["value"], results["confidence"]]
