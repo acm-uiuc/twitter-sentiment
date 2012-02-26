ddaf8c5f3b2954658559438ea7e30c60b8333de4
Jake McGinty <mcginty@illinois.edu>
2012-02-26 17:52:47 -0600

    App skeleton updated with a debugging PrettyPrint library roughed up. Will move to separate module soon.

--- app.rb
+++ app.rb
@@ -1,3 +1,20 @@
 # Add lib/ folder to end of the Ruby library search path so we can simply require them like gems
 $:.push File.join(File.dirname(__FILE__),'lib')
 
+require 'paint'
+require 'twitter-sentiment'
+
+DEBUG = true
+def pp mood=:info, msg=""
+    moods = {
+        :info => Paint["[info] ", [50,50,50]],
+        :debug => Paint["[dbug] ", [87,14,88]],
+        :warn  => Paint["[warn] ", [255,197,44]],
+        :error => Paint["[erro] ", [254,0,106]],
+        :happy => Paint["[ :) ] ", [151,192,12]],
+        :sad   => Paint["[ :( ] ", [171,7,97]]
+    }
+    puts moods[mood] + msg
+end
+
+TwitterSentiment::Input::Twitter.new
