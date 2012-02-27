f050e6ae0f818afa824d503cd740e65abb6a8db9
Jake McGinty <mcginty@illinois.edu>
2012-02-26 20:36:39 -0600

    created PurdyPrint for nice colorful console output

---
+++ lib/purdy_print.rb
@@ -0,0 +1,26 @@
+require 'paint'
+debug = :off # :off, :low, :med, :high
+module PurdyPrint
+    @@debug = :off
+    def level_to_score level
+        level = 0 if level == :off
+        level = 1 if level == :low
+        level = 2 if level == :med
+        level = 3 if level == :high
+        return level
+    end
+    def pp mood=:info, msg="", debug_level=:off
+
+
+        moods = {
+            :info => Paint["[info] ", [50,50,50]],
+            :debug => Paint["[dbug] ", [87,14,88]],
+            :warn  => Paint["[warn] ", [255,197,44]],
+            :error => Paint["[erro] ", [254,0,106]],
+            :happy => Paint["[ :) ] ", [151,192,12]],
+            :sad   => Paint["[ :( ] ", [171,7,97]],
+            :bhargav => Paint["[ :| ] ", [200,200,200]]
+        }
+        puts moods[mood] + msg if level_to_score(debug_level) < level_to_score(@@debug)
+    end
+end
