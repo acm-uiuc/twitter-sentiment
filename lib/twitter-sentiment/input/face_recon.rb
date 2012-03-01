571b4d3f5f73ff6eb280ecc97ff954fd4e2143e3
Jake McGinty <mcginty@illinois.edu>
2012-03-01 15:10:55 -0600

    Fixed naming conventions for facial recognition.

--- lib/twitter-sentiment/input/face_recon.rb
+++ lib/twitter-sentiment/input/face_recon.rb
@@ -15,11 +15,11 @@ module TwitterSentiment
         @client = Face.get_client(:api_key => file[:key], :api_secret => file[:secret])
         pp :info, "Face input initialized successfully."
       end
-      
+
       #Returns Face API info (uses rest API)
       #@param [String] Image URL
       #@return [JSON]
-      def detectFaces imgurl = ""
+      def detect_faces imgurl = ""
         return @client.faces_detect(:urls => [imgurl])
       end
     end #FaceRecon
--- lib/twitter-sentiment/parser/face_recon.rb
+++ lib/twitter-sentiment/parser/face_recon.rb
@@ -1,4 +1,4 @@
-require 'twitter-sentiment/input/face'
+require 'twitter-sentiment/input/face_recon.rb'
 require 'purdy-print'
 
 module TwitterSentiment
@@ -13,11 +13,11 @@ module TwitterSentiment
       #JSON PARSING:
       #results["photos"][photo number]["tags"][face number]["attributes"]["smiling"]["value" or "confidence"]
       
-      #Returns an array of arrays (info about the detected faces)
+      # Returns an array of arrays (info about the detected faces)
       #
-      #@param [FaceAPI search] Image info
-      #@return [[boolean,int]...] Smiling?, confidence
-      def smileInfo info = ""
+      # @param [FaceAPI search] Image info
+      # @return [[boolean,int]...] Smiling?, confidence
+      def smile_info info = ""
         info = info["photos"][0]["tags"]
         arr = []
         info.each do |n|
@@ -27,17 +27,18 @@ module TwitterSentiment
         arr
       end
 
-      #Finds the average happiness of people in profile picture, 
-      #weighted based on confience and number of faces
-      #@param [String] imgURL
-      #@return [float] average happiness
-      def profileImageHappiness img = nil
+      # Finds the average happiness of people in profile picture, 
+      # weighted based on confience and number of faces
+      #
+      # @param [String] imgURL
+      # @return [float] average happiness
+      def profile_image_happiness img = nil
         pp :info, "Getting profileImageHappiness for #{img}."
         if img != nil
-          arr = @client.detectFaces(img) #call whatever calls the FaceAPI
-          arr = smileInfo(arr) #format the search results
+          arr = @client.detect_faces(img) #call whatever calls the FaceAPI
+          arr = smile_info(arr) #format the search results
           return 0 if arr.length == 0
-          #expecting arr = [[boolean,int],[boolean,int].....]
+          # expecting arr = [[boolean,int],[boolean,int].....]
           score = 0
           arr.each do |n|
             s = n[1]
@@ -47,6 +48,6 @@ module TwitterSentiment
           score.to_f / arr.length.to_f
         end # if != nil
       end
-    end #FaceRecon
-  end #Parser
-end #TwitterSentiment
+    end # FaceRecon
+  end # Parser
+end # TwitterSentiment
