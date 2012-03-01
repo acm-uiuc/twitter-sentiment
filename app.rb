ce824ff8179a0348bf79cb1869ed5fdbe956586f
Jake McGinty <mcginty@illinois.edu>
2012-03-01 15:11:19 -0600

    bit more debugging info for app output to control over-information a bit better

--- app.rb
+++ app.rb
@@ -9,23 +9,24 @@ class TwitterBeats
     @@debug = :high # PurdyPrint debug var
 
     def initialize
-        pp :info, "TwitterBeats initializing...", :med
+        pp :info, "TwitterBeats initializing..."
         textmood = TwitterSentiment::Parser::TextMood.new :afinn
         facerecon = TwitterSentiment::Parser::FaceRecon.new
         TwitterSentiment::Input::Twitter.new({
             :status_callback => lambda { |status|
-                                    weight = textmood.score status.text
-                                    mood = :bhargav
-                                    mood = :happy if weight > 0
-                                    mood = :sad if weight < 0
-                                    pp mood, "score: #{weight.to_s.ljust(8)}tweet: #{status.text}"
-                                    imgURL = status.user.profile_image_url
-                                    imgWeight = facerecon.profileImageHappiness(imgURL)
-                                    pp :info, "imgweight received"
-                                    imgmood = :bhargav
-                                    imgmood = :happy if imgWeight > 0
-                                    imgmood = :sad if imgWeight < 0
-                                    pp imgmood, "imgscore: #{imgWeight.to_s.ljust(8)}url: #{imgURL}"
+                                    # text weight
+                                    weight[:text] = textmood.score(status.text)
+                                    mood[:text] = :bhargav
+                                    mood[:text] = :happy if weight[:text] > 0
+                                    mood[:text] = :sad if weight[:text] < 0
+                                    pp mood[:text], "text score: #{weight[:text].to_s.ljust(7)}tweet: #{status.text}", :med
+                                    # image weight
+                                    weight[:img] = facerecon.profileImageHappiness(status.user.profile_image_url)
+                                    pp :info, "face image weight received", :high
+                                    mood[:img] = :bhargav
+                                    mood[:img] = :happy if weight[:img] > 0
+                                    mood[:img] = :sad if weight[:img] < 0
+                                    pp mood[:img], "img score: #{weight[:img].to_s.ljust(8)}url: #{imgURL}", :med
                                 },
         })
     end
