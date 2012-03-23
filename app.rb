0c091bbb350bc0311fad04b6d195539f12b3a33d
Jake McGinty <mcginty@illinois.edu>
2012-03-23 18:56:43 -0500

    app.rb upgraded to be executable and includes the shebang at the top.

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # Add lib/ folder to end of the Ruby library search path so we can simply require them like gems
 $:.push File.join(File.dirname(__FILE__),'lib')
 require 'rubygems'
